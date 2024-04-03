43c14ba6bdbf159596d40b730a9219a1ac8f156d
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-02 20:13:31 -0500

    CLogin was generated

---
+++ JoinResidentialFront/src/app/Components/Login/clogin/clogin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clogin',
+  templateUrl: './clogin.component.html',
+  styleUrls: ['./clogin.component.css']
+})
+export class CloginComponent {
+
+}
